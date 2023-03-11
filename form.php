bf17a3f3e4ba96c98a73d825937ef7824493bd2f
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-11 12:29:21 +0100

    Consegna completata

--- form.php
+++ form.php
@@ -1,3 +1,30 @@
+<?php
+
+// PARAGRAPH PARAMETER CHECK
+
+if (isset($_GET["paragraph"])) {
+    $paragraph = $_GET["paragraph"];
+} else {
+    $paragraph = "";
+}
+
+$badword = $_GET["badword"];
+
+// PARAGRAPH LENGTH
+
+$paragraph_length = strlen($paragraph);
+
+// CENSOR FUNCTION
+
+$censored_paragraph = str_replace($badword, "***", $paragraph);
+
+// CENSORED PARAGRAPH LENGTH
+
+$censored_paragraph_length = strlen($censored_paragraph);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,12 +32,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- BOOTSTRAP -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 <body>
-<h1><?php
-
-echo $_GET["badword"];
-
-?></h1>
+<p><?= $paragraph ?></p>
+<h4><?= $paragraph_length ?></h4>
+<p><?= $censored_paragraph ?></p>
+<h4><?= $censored_paragraph_length ?></h4>
 </body>
 </html>
